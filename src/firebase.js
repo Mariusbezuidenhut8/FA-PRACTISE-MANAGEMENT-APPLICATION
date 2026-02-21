0a0d01f2cc950e1aa5dd5e385b38e608c09736c4
Mariusbezuidenhut8 <133584606+Mariusbezuidenhut8@users.noreply.github.com>
2026-02-21 16:30:46 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_ZQPhaCAYptsadY30BeqKuoTk4g0BltY",
-  authDomain: "fa-practise-management.firebaseapp.com",
-  projectId: "fa-practise-management",
-  storageBucket: "fa-practise-management.firebasestorage.app",
+  apiKey:            "AIzaSyA_ZQPhaCAYptsadY30BeqKuoTk4g0BltY",
+  authDomain:        "fa-practise-management.firebaseapp.com",
+  projectId:         "fa-practise-management",
+  storageBucket:     "fa-practise-management.firebasestorage.app",
   messagingSenderId: "737419772327",
-  appId: "1:737419772327:web:aac63b9ea59ece80dbb682",
-  measurementId: "G-4RGFVW9PVV"
+  appId:             "1:737419772327:web:aac63b9ea59ece80dbb682",
+  measurementId:     "G-4RGFVW9PVV"
 };
 
 const app = initializeApp(firebaseConfig);
